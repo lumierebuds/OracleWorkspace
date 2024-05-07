bb0e59b39bdf476b346f493abbb55b1314448eea
lumierebuds <lumierebuds@gmail.com>
2024-05-07 17:27:46 +0900

    feat:PLSQL practice

---
+++ practice/SQL06_PLSQL.sql
@@ -0,0 +1,54 @@
+
+-- 1.
+DECLARE
+    SAL EMPLOYEE.SALARY%TYPE;
+BEGIN
+    FOR EMP IN (SELECT * FROM EMPLOYEE)
+    LOOP
+        IF EMP.BONUS IS NOT NULL THEN 
+            SAL := NVL((EMP.SALARY + EMP.SALARY * EMP.BONUS) * 12, 0);
+        ELSE 
+            SAL := NVL(EMP.SALARY * 12, 0); 
+        END IF; 
+        
+        DBMS_OUTPUT.PUT_LINE(EMP.EMP_NAME || ' 사원의 연봉은 ' || SAL);
+
+    END LOOP;
+END; 
+/ 
+-- 2. FOR LOOP 
+BEGIN 
+    FOR DAN IN 2..9
+    LOOP
+        FOR NUM IN 1..9 
+        LOOP
+            IF MOD(DAN, 2) = 0 THEN
+               DBMS_OUTPUT.PUT_LINE(DAN || ' X ' || NUM || ' = ' || (DAN * NUM));
+            END IF;
+        END LOOP; 
+    END LOOP;
+END;
+/
+-- 2. WHILE LOOP 
+DECLARE 
+    DAN NUMBER := 2;
+    NUM NUMBER := 1; 
+
+BEGIN 
+    WHILE DAN <10
+    LOOP
+        WHILE NUM < 10
+        LOOP
+            IF MOD(DAN, 2) = 0 THEN 
+            DBMS_OUTPUT.PUT_LINE(DAN || ' X ' || NUM || ' = ' || (DAN * NUM));
+            END IF;
+            NUM := NUM + 1; 
+        END LOOP;
+    DAN := DAN+1;
+    NUM := 1;
+    END LOOP;
+END;
+/
+
+
+SELECT MOD(2, 2) FROM DUAL;
